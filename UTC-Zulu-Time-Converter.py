e52b451554a81a51ae9967b32b76a8df03289c8c
nolassoguy <54215245+nolassoguy@users.noreply.github.com>
2023-01-30 17:37:12 -0600

    Combine & remove 4 lines of code - less legible?

--- UTC-Zulu-Time-Converter.py
+++ UTC-Zulu-Time-Converter.py
@@ -6,12 +6,8 @@ end_time = input('\nPlease enter a UTC-Zulu Date Time Format (e.g. \'2023-01-30T
 
 dt_UTC = dateutil.parser.parse(end_time)
 
-utc_to_central = dt_UTC.astimezone(pytz.timezone('US/Central'))
-utc_to_eastern = dt_UTC.astimezone(pytz.timezone('US/Eastern'))
-utc_to_pacific = dt_UTC.astimezone(pytz.timezone('US/Pacific'))
-
-central_time = datetime.datetime.strftime(utc_to_central,'%A %B %d, %Y ''@'' %I:%M%p') # Formatting USED TO (changed 1/7/2020) match eBay's "Ended: "
-eastern_time = datetime.datetime.strftime(utc_to_eastern,'%A %B %d, %Y ''@'' %I:%M%p')
-pacific_time = datetime.datetime.strftime(utc_to_pacific,'%A %B %d, %Y ''@'' %I:%M%p')
+central_time = datetime.datetime.strftime(dt_UTC.astimezone(pytz.timezone('US/Central')),'%A %B %d, %Y ''@'' %I:%M%p') # Formatting USED TO (changed 1/7/2020) match eBay's "Ended: "
+eastern_time = datetime.datetime.strftime(dt_UTC.astimezone(pytz.timezone('US/Eastern')),'%A %B %d, %Y ''@'' %I:%M%p')
+pacific_time = datetime.datetime.strftime(dt_UTC.astimezone(pytz.timezone('US/Pacific')),'%A %B %d, %Y ''@'' %I:%M%p')
 
 print(f'\nCENTRAL: {central_time}\n\nEASTERN: {eastern_time}\n\nPACIFIC: {pacific_time}\n\n')
