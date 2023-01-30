03471939a59c4343bcb940b214fcad33054ffdd5
nolassoguy <54215245+nolassoguy@users.noreply.github.com>
2023-01-30 13:20:06 -0600

    Remove superfluous code & add Terminal interaction

--- UTC-Zulu-Time-Converter.py
+++ UTC-Zulu-Time-Converter.py
@@ -2,22 +2,16 @@ import datetime
 import dateutil.parser
 import pytz
 
-#3 examples below using the same input for three different timezones
+end_time = input('\nPlease enter a UTC-Zulu Date Time Format (e.g. \'2023-01-30T19:11:05Z\'):\n\n')
 
-end_time = '2020-08-27T01:00:01.000Z'
 dt_UTC = dateutil.parser.parse(end_time)
+
 utc_to_central = dt_UTC.astimezone(pytz.timezone('US/Central'))
-central_time = datetime.datetime.strftime(utc_to_central,'%A %B %d, %Y ''@'' %I:%M%p') # Formatting USED TO (changed 1/7/2020) match eBay's "Ended: "
-print('CENTRAL: ' + central_time) # CENTRAL: Saturday January 04, 2020 @ 08:00PM
+utc_to_eastern = dt_UTC.astimezone(pytz.timezone('US/Eastern'))
+utc_to_pacific = dt_UTC.astimezone(pytz.timezone('US/Pacific'))
 
-end_time = '2020-08-27T01:00:01.000Z'
-dt_UTC = dateutil.parser.parse(end_time)
-utc_to_central = dt_UTC.astimezone(pytz.timezone('US/Eastern'))
 central_time = datetime.datetime.strftime(utc_to_central,'%A %B %d, %Y ''@'' %I:%M%p') # Formatting USED TO (changed 1/7/2020) match eBay's "Ended: "
-print('EASTERN: ' + central_time) # EASTERN: Saturday January 04, 2020 @ 09:00PM
+eastern_time = datetime.datetime.strftime(utc_to_eastern,'%A %B %d, %Y ''@'' %I:%M%p')
+pacific_time = datetime.datetime.strftime(utc_to_pacific,'%A %B %d, %Y ''@'' %I:%M%p')
 
-end_time = '2020-08-27T01:00:01.000Z'
-dt_UTC = dateutil.parser.parse(end_time)
-utc_to_central = dt_UTC.astimezone(pytz.timezone('US/Pacific'))
-central_time = datetime.datetime.strftime(utc_to_central,'%A %B %d, %Y ''@'' %I:%M%p') # Formatting USED TO (changed 1/7/2020) match eBay's "Ended: "
-print('PACIFIC: ' + central_time) # PACIFIC: Saturday January 04, 2020 @ 06:00PM
+print(f'\nCENTRAL: {central_time}\n\nEASTERN: {eastern_time}\n\nPACIFIC: {pacific_time}\n\n')
