04919969d0e4f20f32c004e0b8ec8f950df32733
nolassoguy <54215245+nolassoguy@users.noreply.github.com>
2023-01-31 19:48:03 -0600

    Add regex findall() for user input & try/exception

--- UTC-Zulu-Time-Converter.py
+++ UTC-Zulu-Time-Converter.py
@@ -1,13 +1,22 @@
 import datetime
 import dateutil.parser
 import pytz
+import re
 
 end_time = input('\nPlease enter a UTC-Zulu Date Time Format (e.g. \'2023-01-30T19:11:05Z\'):\n\n')
 
-dt_UTC = dateutil.parser.parse(end_time)
+# https://www.geeksforgeeks.org/pattern-matching-python-regex/
 
-central_time = datetime.datetime.strftime(dt_UTC.astimezone(pytz.timezone('US/Central')),'%A %B %d, %Y ''@'' %I:%M%p') # Formatting USED TO (changed 1/7/2020) match eBay's "Ended: "
-eastern_time = datetime.datetime.strftime(dt_UTC.astimezone(pytz.timezone('US/Eastern')),'%A %B %d, %Y ''@'' %I:%M%p')
-pacific_time = datetime.datetime.strftime(dt_UTC.astimezone(pytz.timezone('US/Pacific')),'%A %B %d, %Y ''@'' %I:%M%p')
+try:
+	zulu_re = re.findall(r'^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\dZ$', end_time)[0]
 
-print(f'\nCENTRAL: {central_time}\n\nEASTERN: {eastern_time}\n\nPACIFIC: {pacific_time}\n\n')
+	dt_UTC = dateutil.parser.parse(zulu_re)
+
+	central_time = datetime.datetime.strftime(dt_UTC.astimezone(pytz.timezone('US/Central')),'%A %B %d, %Y ''@'' %I:%M%p') # Formatting USED TO (changed 1/7/2020) match eBay's "Ended: "
+	eastern_time = datetime.datetime.strftime(dt_UTC.astimezone(pytz.timezone('US/Eastern')),'%A %B %d, %Y ''@'' %I:%M%p')
+	pacific_time = datetime.datetime.strftime(dt_UTC.astimezone(pytz.timezone('US/Pacific')),'%A %B %d, %Y ''@'' %I:%M%p')
+
+	print(f'\nCENTRAL: {central_time}\n\nEASTERN: {eastern_time}\n\nPACIFIC: {pacific_time}\n\n')
+
+except IndexError:
+	print('Sorry, that is not in the correct UTC-Zulu Date Time Format')
